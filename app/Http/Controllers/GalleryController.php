14ac1bf6f1fac05e3183e5ce9624c1d3e347540f
W0lf1n <petr.bohac61@gmail.com>
2022-12-19 21:26:29 +0100

    gallery (pagination) done

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -6,15 +6,34 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Console\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class GalleryController extends Controller
 {
+    /**
+     * @return Application|View|Factory
+     */
     public function index(): Application|View|Factory{
         $req = $this->Svatek();
-        $path = 'public/assets/img/gallery';
-        $images = Storage::files($path);
-        dd($images);
-        return \view('gallery.index', ['day' => $req[1], 'name' => $req[0]]);
+        $file_name_list = array();
+        $images_name = File::files('assets/img/gallery');
+
+
+        /*
+         * Pro budoucí já
+         *      při definování kategorii si vytvoř různé složky, které ty kategorie budou reprezentovat
+         *      pak už budeš jenom zobrazovat fotky, které mají "path" s koncovkou té složky
+         *          (ukázka jak vypadá "path" => assets/img/gallery)
+         */
+
+
+        foreach ($images_name as $img){
+            $file_name_list[] = $img->getFilename();
+        }
+        $page = request('page', 1);
+        $images = collect($file_name_list)->forPage($page, 8);
+        $total_images = ceil(count($file_name_list) / 8);
+        return \view('gallery.index', ['day' => $req[1], 'name' => $req[0], 'images' => $images, 'total_images' => $total_images, 'page' => $page]);
     }
 }
--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -22,119 +22,69 @@
     </div>
 
 
-    <div class="blog-main-area">
+    <div class="blog-main-area mt-30">
         <div class="container container-default custom-area">
-            <div class="row flex-row-reverse">
-                <div class="col-lg-9 col-12 col-custom widget-mt">
-                    <!-- Shop Wrapper Start -->
-                    <div class="row">
-                        <div class="col-12 col-md-6 col-custom mb-30">
-                            <div class="blog-lst">
-                                <div class="single-blog">
-                                    <div class="blog-image">
-                                        <a class="d-block">
-                                            <img src="{{asset('assets/img/gallery/img-1.jpg')}}" alt="Blog Image" class="w-100">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+            <!-- Top Toolbar Start -->
+            <div class="row mb-2">
+                <div class="col-sm-12 col-custom">
+                    <div class="toolbar-bottom">
+                        <div class="pagination">
+                            <ul>
+                                @if($page > 1)
+                                    <li class="previous" ><a href="?page={{$page - 1}}">&lt;&lt; předchozí</a></li>
+                                @endif
+                                @for($pagination = 1; $pagination <= $total_images; $pagination++)
+                                        <li><a href="?page={{$pagination}}">{{$pagination}}</a> </li>
+                                    @endfor
+                                @if($page < $total_images)
+                                    <li class="next ms-2"><a href="?page={{$page + 1}}">další &gt;&gt;</a></li>
+                                @endif
+                            </ul>
                         </div>
-                        <div class="col-12 col-md-6 col-custom mb-30">
-                            <div class="blog-lst">
-                                <div class="single-blog">
-                                    <div class="blog-image">
-                                        <a class="d-block">
-                                            <img src="{{asset('assets/img/gallery/img-2.jpg')}}" alt="Blog Image" class="w-100">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-custom mb-30">
-                            <div class="blog-lst">
-                                <div class="single-blog">
-                                    <div class="blog-image">
-                                        <a class="d-block">
-                                            <img src="{{asset('assets/img/gallery/img-3.jpg')}}" alt="Blog Image" class="w-100">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-custom mb-30">
-                            <div class="blog-lst">
-                                <div class="single-blog">
-                                    <div class="blog-image">
-                                        <a class="d-block">
-                                            <img src="{{asset('assets/img/gallery/img-4.jpg')}}" alt="Blog Image" class="w-100">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-custom mb-30">
-                            <div class="blog-lst">
-                                <div class="single-blog">
-                                    <div class="blog-image">
-                                        <a class="d-block">
-                                            <img src="{{asset('assets/img/gallery/img-5.jpg')}}" alt="Blog Image" class="w-100">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-6 col-custom mb-30">
-                            <div class="blog-lst">
-                                <div class="single-blog">
-                                    <div class="blog-image">
-                                        <a class="d-block">
-                                            <img src="{{asset('assets/img/gallery/img-6.jpg')}}" alt="Blog Image" class="w-100">
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <p class="desc-content text-center text-sm-right mb-0">Stránka {{$page}} - {{$total_images}}</p>
                     </div>
-                    <!-- Shop Wrapper End -->
-                    <!-- Bottom Toolbar Start -->
-                    <div class="row mb-2">
-                        <div class="col-sm-12 col-custom">
-                            <div class="toolbar-bottom">
-                                <div class="pagination">
-                                    <ul>
-                                        <li class="current">1</li>
-                                        <li><a href="#">2</a></li>
-                                        <li><a href="#">3</a></li>
-                                        <li class="next"><a href="#">next</a></li>
-                                        <li><a href="#">&gt;&gt;</a></li>
-                                    </ul>
-                                </div>
-                                <p class="desc-content text-center text-sm-right mb-0">Showing 1 - 12 of 34 result</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Bottom Toolbar End -->
-                </div>
-                <div class="col-lg-3 col-12 col-custom">
-                    <!-- Sidebar Widget Start -->
-                    <aside class="sidebar_widget widget-mt">
-                        <div class="widget_inner">
-                            <div class="widget-list widget-mb-1">
-                                <h3 class="widget-title">Categories</h3>
-                                <div class="sidebar-body">
-                                    <ul class="sidebar-list">
-                                        <li><a href="#">All Product</a></li>
-                                        <li><a href="#">Best Seller (5)</a></li>
-                                        <li><a href="#">Featured (4)</a></li>
-                                        <li><a href="#">New Products (6)</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </aside>
-                    <!-- Sidebar Widget End -->
                 </div>
             </div>
+            <!-- Top Toolbar End -->
+                    <!-- Shop Wrapper Start -->
+                    <div class="row">
+                        @foreach($images as $img)
+                        <div class="col-12 col-md-6 col-custom mb-30">
+                            <div class="blog-lst">
+                                <div class="single-blog">
+                                    <div class="blog-image">
+                                        <a class="d-block">
+                                            <img src="{{asset("assets/img/gallery/$img")}}" alt="gallery image" class="w-100">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <!-- Shop Wrapper End -->
+            <!-- Bottom Toolbar Start -->
+            <div class="row mb-2">
+                <div class="col-sm-12 col-custom">
+                    <div class="toolbar-bottom">
+                        <div class="pagination">
+                            <ul>
+                                @if($page > 1)
+                                    <li class="previous" ><a href="?page={{$page - 1}}">&lt;&lt; předchozí</a></li>
+                                @endif
+                                @for($pagination = 1; $pagination <= $total_images; $pagination++)
+                                    <li><a href="?page={{$pagination}}">{{$pagination}}</a> </li>
+                                @endfor
+                                @if($page < $total_images)
+                                    <li class="next ms-2"><a href="?page={{$page + 1}}">další &gt;&gt;</a></li>
+                                @endif
+                            </ul>
+                        </div>
+                        <p class="desc-content text-center text-sm-right mb-0">Stránka {{$page}} - {{$total_images}}</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Bottom Toolbar End -->
         </div>
     </div>
 
